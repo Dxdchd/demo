7794a15ccf624334e7ce4d3f731a76a2117bd460
DongHyun KIM <105835957+Dxdchd@users.noreply.github.com>
2026-01-31 17:41:54 +0900

    CRUD DONE

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -49,5 +51,36 @@ public class ArticleController {
         model.addAttribute("articleList", articleEntityList);
 
         return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if(target!=null){
+            articleRepository.save(articleEntity);
+        }
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        log.info("del?");
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        if(target!=null){
+            articleRepository.delete(target);
+            redirectAttributes.addFlashAttribute("msg", "article deleted successfully");
+        }
+        return "redirect:/articles";
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(title, content) VALUES ('hi_1', 'content_1');
+INSERT INTO article(title, content) VALUES ('hi_2', 'content_2');
+INSERT INTO article(title, content) VALUES ('hi_3', 'content_3');
